a62fe93f008052f8ab569fe0656203a290826dad
YAdnan408 <yamin.adnan408@gmail.com>
2026-06-06 16:15:42 +0600

    Commit 1: Add more descriptive logging to database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose"
 export const connectDB = async () =>{
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB connected: ${conn.connection.host}`)
+        console.log(`MongoDB connected: ${conn.connection.host}, Database: ${conn.connection.name}`)
     } catch (error) {
         console.log(`Error: ${error.message}`)
         process.exit(1)
